5d2173eb2d3d1f8f69f400cd3ef99d45bd9d2f8e
Ajay lodhi <satyamlodhi123@gmail.com>
2026-05-14 12:35:58 +0530

    wrong code

--- substitution_method.py
+++ substitution_method.py
@@ -1,4 +1,5 @@
 # 1. take values for an equation
+# 1a. add + -
 # 2. take those values and turn it into x and y and any alphabet and constant on right hand sides
 # 2a. isolate x or y
 # 3. put that x or y in second equation and solve it
@@ -6,13 +7,25 @@
 # 5. what if there are more than three equations - allow till three equations
 # 6. use +,-,*,/
 # 7. return a tuple with x and y coordinates
-
+import random
+from operator import add, mul, sub
 
 # 1. take values for an equation
 class Equation:
-    def __init__(self, x=0, y=0, z=0, cons=0):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.cons = cons
-        
+    OPS = {
+        '+' : add,
+        '-' : sub,
+    }
+
+
+    def values(self):
+        a = int(input("Enter Co-efficient for x: "))
+        s = int(input("Enter sign + or -"))
+        b = int(input("Enter Co-efficient for y: "))
+        c = int(input('Enter constant: '))
+
+
+    # def __init__(self):
+    #
+    # def __str__(self):
+    #     return f"{}x"
